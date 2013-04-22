3314436a55ce2e1f851f70eb25d96dfa93d67173
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2013-04-22 12:33:46 +0000

    New Leiningen project Jest.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject jest "0.0.0-SNAPSHOT"
+  :description "Multi touch game for the city of Delft"
+  :url "http://www.github.com/fmsbeekmans/Jest.git"
+  :license {:name "GPL 3 and higher"
+            :url " https://gnu.org/licenses/pgl.html"}
+  :dependencies [[org.clojure/clojure "1.5.0"]])
---
+++ test/jest/core.clj
@@ -0,0 +1,7 @@
+(ns jest.core-test
+  (:use clojure.test
+        jest.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
