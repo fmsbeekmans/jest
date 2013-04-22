3729c3c1b7a3fff9b8ba6c46c5b11947aaec4b52
jlicht <jlicht@aerix.nl>
2013-04-22 13:14:25 +0200

    Renamed core test file, added dependencies for midje testing framework

--- project.clj
+++ project.clj
@@ -2,5 +2,10 @@
   :description "Multi touch game for the city of Delft"
   :url "http://www.github.com/fmsbeekmans/Jest.git"
   :license {:name "GPL 3 and higher"
-            :url " https://gnu.org/licenses/pgl.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]])
+            :url " https://gnu.org/licenses/gpl.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"] ]
+  :profiles {:dev {:plugins [[lein-midje "3.0.0"]]
+                   :dependencies [[midje "1.5.1"]]
+                   }
+             }
+  )
