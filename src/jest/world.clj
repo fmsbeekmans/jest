a9f261fabc07d399293e06bf47e0b3e4d7f987ad
Matthijs van Otterdijk <matko@pievolution.org>
2013-05-06 15:48:00 +0200

    adding paths

--- src/jest/world.clj
+++ src/jest/world.clj
@@ -1,10 +1,22 @@
 (ns jest.world)
 
-(defrecord Cell [x y])
+(defrecord Cell [x y paths])
+
+(defn- path-type [type]
+  `(do (defrecord ~type ~'[in out])
+       (derive ~type ::path)))
+  
+
+(defmacro def-path-types [& types]
+  `(do ~@(map path-type types)))
+
+(def-path-types Road Rails Canal)
+
+(defrecord Path [type in out])
 
 (def ^:dynamic world
-  "Binding containing the world state as a map from [x y] coordinates to cells."
-  {})
+  "Binding containing the world state as an atom containing a map from [x y] coordinates to cells."
+  (atom {}))
 
 (defn world-grid
   "generates and returns a world grid of the given dimension."
@@ -12,30 +24,78 @@
   (reduce conj {}
           (for [x (range sx)
                 y (range sy)]
-            [[x y] (Cell. x y)])))
+            [[x y] (ref (map->Cell {:x x
+                                    :y y
+                                    :roads {}
+                                    :background :none}))])))
 
 (defn cell
   "Returns the cell on the given coordinates"
   [x y]
-  (world [x y]))
+  (@world [x y]))
 
 (defn coords
   "Returns the coordinates of the given cell"
   [c]
   [(:x c) (:y c)])
 
+(defn derefable?
+  "Returns true if the argument can be dereferenced, false otherwise."
+  [x]
+  (instance? clojure.lang.IDeref x))
+
 (defn- direction-m [[dir dx dy]]
   `(defn ~dir
-     ~(format "Returns the cell in direction %s from the given cell." dir)
+     ~(format "Returns the cell in direction %s from the given cell" dir)
      ~'[c]
-     (apply cell (map +
-                      (coords ~'c)
-                      ~[dx dy]))))
+     (if (derefable? ~'c)
+       (recur (deref ~'c))
+       (apply cell (map +
+                        (coords ~'c)
+                        ~[dx dy])))))
 
-(defmacro directions [& dirs]
+(defmacro def-directions [& dirs]
   `(do ~@(map direction-m dirs)))
 
-(directions [north 0 -1]
-            [south 0 1]
-            [west -1 0]
-            [east 1 0])
+(def-directions
+  [north 0 -1]
+  [south 0 1]
+  [west -1 0]
+  [east 1 0])
+
+(defn add-path
+  "adds the given path to the cell"
+  [c type in outs]
+  (update-in c [:paths]
+             (fn [paths]
+               (apply merge
+                      (assoc paths in [:in type])
+                      (map #(assoc paths % [:out type])
+                           outs)))))
+
+;(defn remove-path
+;  "removes the given path from the cell"
+
+(defn in-paths
+  "Returns all incoming paths for the given cell"
+  [c]
+  (for [[dir [inout type]] (:paths c)
+        :when (= :in inout)]
+    [dir type]))
+       
+
+(defn out-paths
+  "Returns all incoming paths for the given cell"
+  [c]
+  (for [[dir [inout type]] (:paths c)
+        :when (= :out inout)]
+    [dir type]))
+
+(defn complete-paths
+  "Returns all in-out path pairs of this cell in the format [type [in out]]"
+  [c]
+  (for [[indir intype] (in-paths c)
+        [outdir outtype] (out-paths c)
+        :when (= intype outtype)]
+    [intype [indir outdir]]))
+        
