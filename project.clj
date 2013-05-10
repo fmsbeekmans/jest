738a5febd79ac8f553de2aaf6174fc6971c6e4a1
Matthijs van Otterdijk <matko@pievolution.org>
2013-05-10 11:51:03 +0200

    updated project.clj to remove unused dependencies and to add documentation plugin.

--- project.clj
+++ project.clj
@@ -3,11 +3,9 @@
   :url "http://www.github.com/fmsbeekmans/Jest.git"
   :license {:name "GPL 3 and higher"
             :url " https://gnu.org/licenses/gpl.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [YAJTL/YAJTL "0.0.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :repositories {"pievolution" "http://pievolution.org/maven-repo/"}
-  :profiles {:dev {:plugins [[lein-midje "3.0.0"]]
-                   :dependencies [[midje "1.5.1"]]
-                   }
-             }
-  )
+  :profiles {:dev {:plugins [[lein-midje "3.0.0"]
+                             [codox "0.6.4"]]
+                   :dependencies [[midje "1.5.1"]]}})
+
