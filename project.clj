5c9757d86d2e155bb3a536a93760ba948ea1d5af
Matthijs van Otterdijk <matko@pievolution.org>
2013-05-10 13:37:46 +0200

    added documentation directory, documentation links to github sources

--- project.clj
+++ project.clj
@@ -7,5 +7,8 @@
   :repositories {"pievolution" "http://pievolution.org/maven-repo/"}
   :profiles {:dev {:plugins [[lein-midje "3.0.0"]
                              [codox "0.6.4"]]
-                   :dependencies [[midje "1.5.1"]]}})
+                   :dependencies [[midje "1.5.1"]]}}
+  :codox {:src-dir-uri "https://github.com/fmsbeekmans/jest/blob/world-state"
+          :src-linenum-anchor-prefix "L"})
+             
 
