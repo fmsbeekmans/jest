38fe1c5dbd0c7fb18cb08126a54b9758a5912896
Matthijs van Otterdijk <matko@pievolution.org>
2013-05-10 11:50:40 +0200

    added documentation for maybe-deref

--- src/jest/util.clj
+++ src/jest/util.clj
@@ -5,7 +5,9 @@
   [x]
   (instance? clojure.lang.IDeref x))
 
-(defn maybe-deref [x]
+(defn maybe-deref
+  "returns the derefed x if x is a derefable. if x is not a derefable, it is returned directly."
+  [x]
   (if (derefable? x)
     (deref x)
     x))
