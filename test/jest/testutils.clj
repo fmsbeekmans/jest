053f6608ec6ea9523ee1748c2de028576fa0fad8
Matthijs van Otterdijk <matko@pievolution.org>
2013-05-22 16:00:45 +0200

    Rewrote tests for paths

--- test/jest/testutils.clj
+++ test/jest/testutils.clj
@@ -1,5 +1,6 @@
 (ns jest.testutils
-  (:require [jest.world.cell :as cell]))
+  (:require [jest.world.cell :as cell])
+  (:use midje.sweet))
 
 (defmacro for-cells
   "test macro, give sx and sy and it will do something for each cell."
@@ -9,3 +10,6 @@
             (let [~cell (cell/cell [~x ~y])]
               ~@body))))
 
+(defmacro world-fact [[sx sy] fact-text & body]
+  `(cell/with-initialized-temp-world [~sx ~sy]
+     (fact ~fact-text ~@body)))
--- test/jest/world/path_test.clj
+++ test/jest/world/path_test.clj
@@ -5,31 +5,35 @@
   (:require [jest.world.path :as path]))
 
 
-(with-initialized-temp-world [10 10]
-  (fact "No paths should exist after world initialization."
-        (doall (map (fn [c]
-                      (and (empty? (path/in-paths c))
-                           (empty? (path/out-paths c))))
-                    (all-cells))) => (n-of true 100))
+(world-fact [10 10]
+            "No paths should exist after world initialization."
+            (doall (map (fn [c]
+                          (and (empty? (path/in-paths c))
+                               (empty? (path/out-paths c))))
+                        (all-cells))) => (n-of true 100))
 
-  (fact "After making a path, it exists in the right direction, in two adjacent cells."
-        (path/build-path (cell [2 3]) :east :road)
-        (path/out-paths (cell [2 3])) => (contains (fn [path]
-                                                     (= (:direction path) :east)))
-        (path/in-paths (cell [3 3])) => (contains (fn [path]
-                                                     (= (:direction path) :west))))
+(world-fact [10 10]
+            "After making a path, it exists in the right direction, in two adjacent cells."
+            (path/build-path (cell [2 3]) :east :road)
+            (path/path (cell [2 3]) :east) => TRUTHY
+            (path/path (cell [3 3]) :west) => TRUTHY)
 
-  (fact "After unbuilding a path, it no longer exists."
-        (path/unbuild-path (cell [2 3]) :east)
-        (path/out-paths (cell [2 3])) =not=> (contains (fn [path]
-                                                         (= (:direction path) :east)))
-        (path/in-paths (cell [3 3])) =not=> (contains (fn [path]
-                                                        (= (:direction path) :west))))
+(world-fact [10 10]
+            "After unbuilding a path, it no longer exists."
+            (path/build-path (cell [2 3]) :east :road)
+            (path/unbuild-path (cell [2 3]) :east)
+            (path/path (cell [2 3]) :east) => FALSEY
+            (path/path (cell [3 3]) :west) => FALSEY)
 
-  (fact "Paths can be unbuilt in both directions."
-        (path/build-path (cell [2 3]) :east :road)
-        (path/unbuild-path (cell [3 3]) :west)
-        (path/out-paths (cell [2 3])) =not=> (contains (fn [path]
-                                                         (= (:direction path) :east)))
-        (path/in-paths (cell [3 3])) =not=> (contains (fn [path]
-                                                        (= (:direction path) :west)))))
+(world-fact [10 10]
+            "Paths can be unbuilt from the adjacent cell as well."
+            (path/build-path (cell [2 3]) :east :road)
+            (path/unbuild-path (cell [3 3]) :west)
+            (path/path (cell [2 3]) :east) => FALSEY
+            (path/path (cell [3 3]) :west) => FALSEY)
+
+(world-fact [10 10]
+            "A built path is an out-path in the cell it was built in, and an in-path in the adjacent cell."
+            (path/build-path (cell [2 3]) :east :road)
+            (path/out-path? (path/path (cell [2 3]) :east)) => TRUTHY
+            (path/in-path? (path/path (cell [3 3]) :west)) => TRUTHY)
