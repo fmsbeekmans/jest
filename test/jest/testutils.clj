d8fe888a0a3842764954bddb0c83d86cbe10f089
Matthijs van Otterdijk <matko@pievolution.org>
2013-05-24 14:51:08 +0200

    function to create a circular path to/from a cell.

--- test/jest/testutils.clj
+++ test/jest/testutils.clj
@@ -1,6 +1,7 @@
 (ns jest.testutils
-  (:require [jest.world.cell :as cell])
-  (:use midje.sweet))
+  (:use [jest.world.cell :only [cell]]
+        [jest.world.path :only [build-path complete-paths]]
+        [jest.world.building :only [build-spawn]]))
 
 (defmacro for-cells
   "test macro, give sx and sy and it will do something for each cell."
@@ -13,3 +14,13 @@
 (defmacro world-fact [[sx sy] fact-text & body]
   `(cell/with-initialized-temp-world [~sx ~sy]
      (fact ~fact-text ~@body)))
+
+(defn build-spawn-circle []
+  {:post [(seq (complete-paths (cell [5 5])))]}
+  (build-spawn (cell [5 5]) :fireball)
+  (build-path (cell [5 5]) :east :pipe)
+  (build-path (cell [6 5]) :south :pipe)
+  (build-path (cell [6 6]) :west :pipe)
+  (build-path (cell [5 6]) :west :pipe)
+  (build-path (cell [4 6]) :north :pipe)
+  (build-path (cell [4 5]) :east :pipe))
