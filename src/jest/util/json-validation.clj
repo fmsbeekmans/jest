a8d9a7c5c972abb5d122de1520b9cda5f710ee39
jlicht <jlicht@aerix.nl>
2013-05-24 10:22:04 +0200

    final version now working with clojure data structures

--- src/jest/util/json-validation.clj
+++ src/jest/util/json-validation.clj
@@ -3,24 +3,32 @@
   (:require [clojure.data.json :as json])
   (:import com.fasterxml.jackson.databind.JsonNode
            [com.github.fge.jsonschema.main JsonSchema JsonSchemaFactory]
-           com.fasterxml.jackson.databind.node.JsonNodeFactory
            com.github.fge.jackson.JsonLoader
-           com.github.fge.jsonschema.report.ProcessingReport
-           ))
+           com.github.fge.jsonschema.report.ProcessingReport))
+
+(defn- JsonNode->data
+  "Converts a jackson JsonNode to clojure data"
+  [node]
+  (json/read-str (.toString node)
+                 :key-fn keyword))
+
+(defn- data->JsonNode
+  "Converts clojure data to a jackson JsonNode"
+  [data]
+  (JsonLoader/fromString
+   (json/write-str data)))
 
 (defn validator
-  "Parses a Json-resource as a Json-schema, and returns a function that
-  validates Json-resources for conformance to the schema"
-  [json-resource]
+  "Parses a Json-schema, and returns a function that
+  validates Json-data for conformance to the schema"
+  [json-schema]
   (let [fact (JsonSchemaFactory/byDefault)
-        schema (.getJsonSchema fact json-resource)]
-      #(.validate schema %1)))
-
-(defn json-resource
-  "Parses a Json file and returns the JsonNode"
-  [path]
-  (JsonLoader/fromPath path))
-
-(defn json->JsonNode
-  [json]
-  2)
+        schema (.getJsonSchema fact (data->JsonNode json-schema))]
+      (fn [json-data on-success on-failure]
+        (let [report (.validate schema (data->JsonNode json-data))
+              success? #(.isSuccess report)
+              report-json (JsonNode->data (.asJson report))]
+          (apply #(partial %1 %2)
+                 (if (success?)
+                   [on-success json-data]
+                   [on-failure report-json]))))))
