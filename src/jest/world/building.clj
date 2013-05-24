17d46098befd41e2b97665ade1d400929cc28772
Matthijs van Otterdijk <matko@pievolution.org>
2013-05-24 14:03:51 +0200

    Removed coordinate version of the build and unbuild functions for buildings.

--- src/jest/world/building.clj
+++ src/jest/world/building.clj
@@ -63,11 +63,8 @@
 
        (defn ~build
          ~(format "Alters world state by building a %s to the given cell.\n" type)
-         ([~'c ~@field-syms]
-            (dosync (alter-cell ~'c ~add ~@field-syms)))
-         ([~'x ~'y ~@field-syms]
-            (~build (cell ~'x ~'y) ~@field-syms)))
-
+         [~'c ~@field-syms]
+         (dosync (alter-cell ~'c ~add ~@field-syms)))
 
        (defn- ~remove
          ~(format "removes a %s from the given cell." type)
@@ -78,10 +75,8 @@
 
        (defn ~unbuild
          ~(format "Alters world state by unbuilding a %s from the given cell." type)
-         ([~'c]
-            (dosync (alter-cell ~'c ~remove)))
-         ([~'x ~'y]
-            (~unbuild (cell ~'x ~'y))))
+         [~'c]
+         (dosync (alter-cell ~'c ~remove)))
 
        (defn ~all
          ~(format "returns all cells with building type %s." type)
