fa8af59df4e4dc64ff054c71d39cc1e3272afdf4
Matthijs van Otterdijk <matko@pievolution.org>
2013-06-07 15:46:11 +0200

    hue multimethod

--- src/jest/color.clj
+++ src/jest/color.clj
@@ -1,30 +1,30 @@
 (ns jest.color)
 
-(defn hue->radian [h]
-	(/ (* (mod h 256) 2 Math/PI)
-	   256))
+(defn hue->vector [h]
+  [(Math/cos h)
+   (Math/sin h)])
 
-(defn radian->vector [r]
-  [(Math/cos r)
-   (Math/sin r)])
-
-(def hue->vector (comp radian->vector hue->radian))
-
-(defn vector->radian [[x y]]
+(defn vector->hue [[x y]]
   (let [atan (Math/atan2 y x)]
     (if (>= atan 0)
       atan
       (+ atan (* 2 Math/PI)))))
 	   
-(defn radian->hue [r]
-  (mod  (Math/round  (* 256
-                        (/ r (* 2 Math/PI))))
-        256))
-
-(def vector->hue (comp radian->hue vector->radian))
-
-
 (defn average-hue [h1 h2]
   (vector->hue (map +
                       (hue->vector h1)
                       (hue->vector h2))))
+
+(defmulti hue type)
+
+(defmethod hue Number [deg]
+  (Math/toRadians deg))
+
+(def colors
+  {:red (hue 0)
+   :yellow (hue 60)
+   :green (hue 120)
+   :blue (hue 240)}) 
+
+(defmethod hue clojure.lang.Keyword [color-name]
+  (colors color-name))
