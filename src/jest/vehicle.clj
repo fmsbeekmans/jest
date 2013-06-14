f6be27b1d6a24844134db1fdc584cddd96420866
Matthijs van Otterdijk <matko@pievolution.org>
2013-06-14 17:04:18 +0200

    Vehicle documentation

--- src/jest/vehicle.clj
+++ src/jest/vehicle.clj
@@ -1,4 +1,5 @@
 (ns jest.vehicle
+  "Code for vehicle manipulation"
   (:use [jest.world :only [cell alter-cell coords all-cells]]
         [jest.world.building :only [vehicle-type spawn?]]
         [jest.world.path :only [in-paths out-paths from to path-type vehicle->path path->duration path opposite-dirs]]
@@ -7,20 +8,30 @@
 
 (defrecord Vehicle [id type coords entry-time entry-direction exit-time exit-direction cargo state])
 
-(def cargo-capacity {:truck 1
-                     :train 5
-                     :boat 10})
+(def ^{:doc "Returns how much cargo each vehicle type can carry"}
+  cargo-capacity {:truck 1
+                  :train 5
+                  :boat 10})
 
-(defn cargo-color [v] (first (:cargo v)))
-(defn cargo-count [v] (or (second (:cargo v)) 0))
+(defn cargo-color
+  "Returns the color of the cargo a vehicle is carrying.
+   This is a hue color code, as a Double."
+  [v]
+  (first (:cargo v)))
+(defn cargo-count
+  "Returns the amount of cargo a vehicle is carrying."
+  [v]
+  (or (second (:cargo v)) 0))
 
-(defn vehicle->duration [v]
+(defn vehicle->duration
+  "Returns the amount of time the given vehicle should spend in a cell."
+  [v]
   (path->duration (vehicle->path (:type v))))
   
 (defn vehicles
-"Returns the vehicles on the given cell."
-[c]
-(:vehicles c))
+  "Returns the vehicles on the given cell."
+  [c]
+  (:vehicles c))
 
 (defn vehicle-cell
   "Returns the cell this vehicle is on"
@@ -28,6 +39,7 @@
   (cell (:coords v)))
 
 (defn vehicle
+  "Returns the vehicle with the given id, or nil if there isn't any."
   ([id]
      (first (remove nil? (map #(vehicle % id)
                               (all-cells)))))
@@ -36,6 +48,9 @@
                     (:vehicles c)))))
 
 (defn update-vehicle
+  "Updates the vehicle with the given id.
+   Function f is called with the vVhicle record and the given args as arguments.
+   The result of this function should be the new Vehicle record."
   [id f & args]
   (let [v (vehicle id)
         nv (apply f v args)]
@@ -43,7 +58,10 @@
                 update-in [:vehicles] #(conj (disj % v)
                                              nv))))
 
-(defn vehicle-state-change [id state]
+(defn vehicle-state-change
+  "Updates the vehicle with the given id to the given state.
+   State should be a keyword, one of :spawning, :despawning, :moving, :exploding"
+  [id state]
   (update-vehicle id assoc :state state))
 
 (defn load-vehicle
@@ -64,28 +82,41 @@
   (assoc v :coords nil))
 
 
-(defn- =state [id state]
+(defn- =state
+  "Compares the state of the vehicle with the given id to the given state."
+  [id state]
   (= (:state (vehicle id))
      state))
 
-(defn spawning? [id]
+(defn spawning?
+  "Returns true iff the vehicle with the given id is in the spawning state."
+  [id]
   (=state id :spawning))
 
-(defn despawning? [id]
+(defn despawning?
+  "Returns true iff the vehicle with the given id is in the despawning state."
+  [id]
   (=state id :despawning))
 
-(defn cargo? [vehicle]
+(defn cargo?
+  "Returns true iff the given vehicle has cargo"
+  [vehicle]
   (not (not (:cargo vehicle))))
 
 (defn set-cargo
+  "Updates the vehicle with the given id to have resource-count cargo of color
+   resource-color, which should be a hue."
   [id resource-color resource-count]
   (update-vehicle id
                   assoc :cargo [resource-color resource-count]))
 
 (defn clear-cargo
+  "Updates the vehicle with the given id to no longer have any cargo."
   [id]
   (update-vehicle id
                   assoc :cargo nil))
 
-(defn all-vehicles []
+(defn all-vehicles
+  "Returns all vehicles in the game."
+  []
   (remove nil? (flatten (map (comp seq vehicles) (all-cells)))))
