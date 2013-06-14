8d9052cd6fed5fdc33a77a3d2d0f00ef0c7a53c2
jlicht <jlicht@aerix.nl>
2013-06-14 10:07:58 +0200

    docstrings for tiled validation

--- src/jest/tiled/validation.clj
+++ src/jest/tiled/validation.clj
@@ -1,5 +1,5 @@
 (ns jest.tiled.validation
-  "Functions for importing tiled maps in the JSON format"
+  "Functions for validating game-levels"
   (:require [clojure.data.json :as json]
             [jest.util :as util]
             [jest.util.json-validation :as json-val]
@@ -10,16 +10,27 @@
 ; with keywords for keys.
 
 (defn read-json
+  "Reads json-data as a clojure data-structure from json-path, which
+  should be a valid path or URL object"
   [json-path]
   (json/read-str (slurp json-path)
                  :key-fn keyword))
 
-(def meta-schema-url (clojure.java.io/resource "meta.schema"))
-(def level-schema-url (clojure.java.io/resource "level.schema"))
+(def meta-schema-url
+  "Location of the schema-validating schema"
+  (clojure.java.io/resource "meta.schema"))
 
-(defn create-validator [schema f]
+(def level-schema-url
+  "Location of the level-validating schema"
+  (clojure.java.io/resource "level.schema"))
+
+(defn create-validator
+  "Constructs a validator fn for schema-path by iff the application of
+  f to the internal json representation of the schema-file results in
+  a not-nil value. The validator expects a json data-structure"
+  [schema-path f]
   (let [level-validator
-        (-?> schema
+        (-?> schema-path
              read-json
              f
              json-val/validator
