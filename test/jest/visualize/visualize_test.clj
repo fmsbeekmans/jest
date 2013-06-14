43a0ac18a5e98bfd4169f3baa9f8a41561ff3f69
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2013-06-14 10:27:53 +0000

    Indentation.

--- test/jest/visualize/visualize_test.clj
+++ test/jest/visualize/visualize_test.clj
@@ -6,11 +6,11 @@
             [jest.world.building :as building]))
 
 (world-fact [10 10]
-            "layer from world state builds a valid grid drawable"
-            (build-spawn-circle)
-            (let [grid (visualize/world-state->Grid (fn [c]
-                                                         (if (building/spawn? c)
-                                                           (drawable/->Stack [])
-                                                           (drawable/->Nothing))))]
-              (get-in grid [:grid [5 5]]) => (drawable/->Stack [])
-              (get-in grid [:grid [5 6]]) => (drawable/->Nothing)))
+  "layer from world state builds a valid grid drawable"
+  (build-spawn-circle)
+  (let [grid (visualize/world-state->Grid (fn [c]
+                                            (if (building/spawn? c)
+                                              (drawable/->Stack [])
+                                              (drawable/->Nothing))))]
+    (get-in grid [:grid [5 5]]) => (drawable/->Stack [])
+    (get-in grid [:grid [5 6]]) => (drawable/->Nothing)))
