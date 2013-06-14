1a88a23fa32d5b18e7b48d393d7d6cfac2baac17
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2013-06-14 09:51:15 +0000

    Documented points.

--- src/jest/visualize/points.clj
+++ src/jest/visualize/points.clj
@@ -1,20 +1,36 @@
 (ns jest.visualize.points)
 
+;; Multi methods for strokes.
+
 (defmulti length
+  "Return the length of a stroke based on it's type."
   (fn [s]
     (:stroke-type (meta s))))
 
 (defmulti tangent
+  "Return the tangent of a stroke at progress p from dimensions d1 to d2."
   (fn [s p [d1 d2]]
     (:stroke-type (meta s))))
 
 (defmulti point
+  "Return a point at a certain progress in a stroke."
   (fn [s p]
     (:stroke-type (meta s))))
 
+;; Helper functions
+
+(defn start-point [l]
+  "Short hand for the point at the start."
+  (point l 0))
+
+(defn end-point [l]
+  "Short hand for point at the end."
+  (point l 1))
+
+;; Simple stroke
+
 (defn stroke
-  #^{:doc (str "Returns a 1-arity function that calculates "
-               "an n-dimentional point at pro")}
+  "Returns a linear stroke between the n-dimentional points from and to."
   [from to]
   {:pre [(= (count from) (count to))
          (not= from to)
@@ -32,15 +48,10 @@
                   to))))
     {:stroke-type :simple}))
 
-(defn start-point [l]
-  (point l 0))
-
-(defn end-point [l]
-  (point l 1))
-
 (defmethod length
   :simple
   [l]
+  "The length of a simple strqke."
   (Math/sqrt
    (apply +
           (map (fn [p1 p2]
@@ -52,11 +63,13 @@
 (defmethod point
   :simple
   [s p]
+  "Point on a simple stroke at progress p."
   (s p))
 
 (defmethod tangent
   :simple
   ([l _ [d1 d2]]
+     "Tangent on a single stroke from dimension d1 to d2, progress is th irrelevant."
       (let [p1 (start-point l)
             p2 (l 1)
             dx (- (p2 d1)
@@ -66,8 +79,11 @@
         (Math/atan2 dy
                     dx))))
 
+;; Composed
+
 (defn strokes-connected?
   [ss]
+  "Does each stroke start at the end of the stroke before it?"
   (if (seq ss)
     (loop [stroke (first ss)
            last-p (start-point stroke)
@@ -83,10 +99,15 @@
                  (rest ss'))
           false)
         true))
-    false))
+    true))
 
 (defn index-sub-strokes
   [ss]
+  #^{:doc (str "Make a map of a vector of strokes, where the key "
+               "is a relative progress interval and as value a map of "
+               "offset, howmuch stroke comes before this starts in absolute? "
+               "progress, relative, how long is this sub-stroke? "
+               "and the stroke itself.")}
   {:pre [(strokes-connected? ss)]}
   (let [total-length (apply + (map length ss))]
     (loop [sum 0
@@ -107,6 +128,7 @@
 
 (defn sub-stroke
   [ss p]
+  "At which sub-stroke is p?"
   {:pre [(= (:stroke-type (meta ss)) :composed)]}
   (first
    (if (= 0 p)
@@ -125,25 +147,16 @@
 
 (defn stroke-comp
   [ss]
+  "Compose strokes in order, return a front-end that let's them behave as one."
   {:pre [(strokes-connected? ss)]}
   (with-meta
-    (fn [p]
-      (start-point (first ss))
-      (first (keep (fn [[[start end] {offset :offset
-                                     p' :progress
-                                     stroke :stroke
-                                     }]]
-                     (if (and
-                          (> p start)
-                          (<= p end))
-                       (stroke (/ (- p offset)
-                                  p'))))
-                   (index-sub-strokes ss))))
+    ss
     {:stroke-type :composed
      :indexed-sub-strokes (index-sub-strokes ss)}))
 
 (defmethod point :composed
   [s p]
+  "Where is p on a composed stroke?"
   (let [{offset :offset
          p' :progress
          sub-stroke :stroke} (sub-stroke s p)]
@@ -152,11 +165,13 @@
 
 (defmethod length :composed
   [ss]
+  "How long are all the sub-strokes of this stroke combined?"
   (apply + (map length
                 (map :stroke (vals (:indexed-sub-strokes (meta ss)))))))
 
 (defmethod tangent :composed
   [ss p [d1 d2]]
+  "What's the tangent of this stroke at p from d1 to d2?"
   (let [{offset :offset
          p' :progress
          sub-stroke :stroke} (sub-stroke ss p)]
