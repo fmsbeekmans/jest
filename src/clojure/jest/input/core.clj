68fcc1413051ed12c22eb802330f06d75df965ae
Matthijs van Otterdijk <matko@pievolution.org>
2013-06-25 11:45:37 +0200

    dumb interpolation if a move event is received for a non-adjacent tile.

--- src/clojure/jest/input/core.clj
+++ src/clojure/jest/input/core.clj
@@ -44,9 +44,31 @@
   (on-up-handler id (@pointers id))
   (swap! pointers dissoc id))
 
+(defn step [a b]
+  (cond (= a b) 0
+        (neg? (- b a)) -1
+        :default 1))
+
+;;dumbly interpolates by first moving to the right x, then to the right y
+(defn- interpolate [t1 [x y]]
+  (loop [[px py] t1
+         acc []]
+    (let [sx (step px x)
+          sy (if (= sx 0)
+               (step py y)
+               0)
+          next (map + [px py] [sx sy])]
+      (if (= [0 0] [sx sy])
+        acc
+        (recur next (conj acc next))))))
+
 (defn receive-move [id p]
   (with-tile [t p]
     (let [prev (@pointers id)]
       (when-not (= prev t)
-        (swap! pointers assoc id t)
-        (on-move-handler id prev t)))))
+        (loop [prev prev
+               [t & tnext] (interpolate prev t)]
+          (swap! pointers assoc id t)
+          (on-move-handler id prev t)
+          (if tnext
+            (recur t tnext)))))))
