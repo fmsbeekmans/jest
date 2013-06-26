82a2a2b61676b9c89fe8f1a6f9f68c55bddafea5
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2013-06-26 15:07:30 +0200

    Functions to bridge between world-state and points.

--- src/clojure/jest/visualize/util.clj
+++ src/clojure/jest/visualize/util.clj
@@ -2,7 +2,9 @@
   (:require [jest.world :as world]
             [jest.vehicle :as vehicle]
             [jest.movement :as movement]
-            [jest.world.cell :as cell])
+            [jest.world.cell :as cell]
+            [jest.scheduler :as scheduler])
+  (:require [jest.visualize.points :as points])
   (:require [quil.core :as quil]
             [brick.drawable :as drawable]))
 
@@ -28,6 +30,35 @@
      :west [(first h-range)
              (second center)]}))
 
-(defn vehicle->stroke [v])
+(defn vehicle->stroke-to-mid
+  [v]
+  (let [points (cell-points (world/cell (:coords v)))]
+    (points/stroke (points (:entry-direction v))
+                   (points :center))))
 
-(defn vehicle->progress [v])
+(defn vehicle->stroke-from-mid
+  [v]
+  (let [points (cell-points (world/cell (:coords v)))]
+    (points/stroke (points :center)
+                   (points (:exit-direction v)))))
+
+(defn vehicle->stroke
+  [v]
+  (cond
+   (and (:entry-direction v)
+        (not (:exit-direction v))) (vehicle->stroke-to-mid v)
+   (and (:exit-direction v)
+        (not (:entry-direction v))) (vehicle->stroke-from-mid v)
+   :default (points/stroke-comp [(vehicle->stroke-to-mid v)
+                                 (vehicle->stroke-from-mid v)])))
+
+
+(defn vehicle->progress [v]
+  (let [duration (- (:exit-time v)
+                    (:entry-time v))
+        elapsed (- @scheduler/game-time
+                   (:entry-time v))]
+    (cond (> elapsed duration) 1
+          (< elapsed 0) 0
+          :default (/ elapsed
+                      duration))))
