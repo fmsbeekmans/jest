8a0d5747b6ab9c4dcd1daae692b9395fe70dda5e
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2013-06-26 15:11:16 +0200

    Cleaned up visualize.clj

--- src/clojure/jest/visualize/visualize.clj
+++ src/clojure/jest/visualize/visualize.clj
@@ -60,91 +60,9 @@ cell-draw-fn is a function that returns a Drawable."
           (for [c (world/all-cells)]
             [(world/coords c)  (cell-draw-fn c)])))))
 
-
-(defn cell-borders
-  [c]
-  (let [h-range ((drawable/ranges
-                  (world/world-width)
-                  (quil.core/width)) ((world/coords c) 0))
-        v-range ((drawable/ranges
-                  (world/world-height)
-                   (quil.core/height)) ((world/coords c) 1))]
-    {:north (first v-range)
-     :west (first h-range)
-     :south (apply + v-range)
-     :east (apply + h-range)}))
-
-(defn cell-center
-  "Returns the center of a cell."
-  [c]
-  (let [h-range ((drawable/ranges
-                  (world/world-width)
-                  (quil.core/width)) ((world/coords c) 0))
-        v-range ((drawable/ranges
-                  (world/world-height)
-                  (quil.core/height)) ((world/coords c) 1))]
-    [(/ (apply + h-range) 2)
-     (/ (apply + v-range) 2)]))
-
-(defn v-point
-  [center borders dir]
-  [(first center) (borders dir)])
-
-(defn h-point
-  [center borders dir]
-  [(borders dir) (second center)])
-
-(defn point-from-center
-  [center borders dir]
-  ((if (or
-         (= dir :east)
-         (= dir :west))
-      h-point
-      v-point) center borders dir))
-
-;; from en to in coords zijn de cell-coordinaten, dit moeten de game
-;; coordinaten zijn
-(defn vehicle->stroke
-  "Geef dirs op"
-  [v coords from to]
-  (cond
-   (vehicle/spawning? (:id v))
-   (points/stroke (coords :center)
-                  (coords :to))
-    (vehicle/moving? (:id v))
-    (let [l1 (points/stroke (point-from-center (coords :center)
-                                               (coords :borders)
-                                               :north) ;;; from
-                            (coords :center))
-          l2 (points/stroke (coords :center) ; goede coord v/h
-                            (point-from-center (coords :center)
-                                               (coords :borders)
-                                               :east))] ;;; to
-      (points/stroke-comp [l1 l2]))
-    (vehicle/despawning? (:id v))
-    (points/stroke (coords :center)
-                   (coords :to))))
-
 (defn vehicle->location
   [v]
-  (let [coords (:coords v)
-        borders (cell-borders (world/cell coords))
-        center (cell-center (world/cell coords))
-        stroke (vehicle->stroke
-                v
-                {:center center
-                 :to coords
-                 :borders borders}
-                (:entry-direction v)
-                (:exit-direction v))
-        t 0
-        ;;(/ (- (:exit-)))
-        ;; duur
-        ;; game-time - entry-time
-           ]
-    (let [[x y] (points/point stroke t)]
-      [(/ x (quil.core/width))
-       (/ y (quil.core/height))])))
+)
 
 (defn vehicles->Stack
   [vehicle-type image]
@@ -155,30 +73,6 @@ cell-draw-fn is a function that returns a Drawable."
                                 (vehicle->location v)
                                 (vehicle-scale) Math/PI))
      (vehicle/all-vehicles vehicle/truck?)))))
-
-(defn vehicle-picker [cell]
-  (if-let [v (first (vehicle/vehicles cell))]
-    (or (vehicle/cargo-color v)
-        :no-cargo)))
-
-(defn draw-vehicle [color w h]
-  (when color
-    (quil.core/color-mode :hsb)
-    (if (= color :no-cargo)
-      (quil.core/fill 0 0 0)
-      (quil.core/fill (* color
-                         (/ (* 2 Math/PI))
-                         255)
-                      255
-                      255))
-    (quil.core/ellipse 32 32 20 20)
-    (quil.core/color-mode :rgb)))
-
-
-(defn vehicle-f [color]
-  (reify brick.drawable.Drawable
-    (draw [this [w h]]
-      (draw-vehicle color w h))))
 
 (defn world->drawable
   [bg-fn building-fn path-fn vehicle-fn]
@@ -208,13 +102,6 @@ cell-draw-fn is a function that returns a Drawable."
           :supply building/resource-type
           :depot building/resource-type} type) c)))))
 
-(defn cell-canal
-  ""
-  [c]
-    (if (seq (path/canals c))
-      (drawable/->Image (image/path->PImage (clojure.java.io/resource "canal.png")))
-      (drawable/->Nothing)))
-
 (defn cell-road
   "Return the appropriate tile-key for roads in this cell."
   [c]
@@ -229,19 +116,6 @@ cell-draw-fn is a function that returns a Drawable."
     ;; temp
     (if-not (empty? (first out))
       (hyphenate-keywords :road (:direction (first out))))))
-
-(defn cell-rails [c]
-  (if (seq (filter (fn [[_ v]]
-                (= :rails (:type v))) (:paths c)))
-    (drawable/->Image (image/path->PImage (clojure.java.io/resource "rails.png")))
-    (drawable/->Nothing)))
-
-(defn world->sketch []
-  (reify drawable/Drawable
-    (draw [this [w h]]
-      ;maak een drawable van de wereld
-      ;teken deze
-      (.draw (world->drawable) [w h]))))
 
 (defn setup [tile-f]
   ;init een bricklet met tile-set
