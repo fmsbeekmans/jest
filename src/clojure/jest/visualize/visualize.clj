d1771d38e8780399aeef6666c4d56c05aaf3fb26
jlicht <jlicht@aerix.nl>
2013-07-09 14:04:21 +0200

    added more fps

--- src/clojure/jest/visualize/visualize.clj
+++ src/clojure/jest/visualize/visualize.clj
@@ -306,6 +306,7 @@ cell-draw-fn is a function that returns a Drawable."
                    [w h]))))
              (atom [])
              :renderer :java2d
+             :init (fn [_] (quil/frame-rate 60))
              :size [800 600]
              :mouse-pressed input/on-down-handler
              :mouse-released input/on-up-handler
