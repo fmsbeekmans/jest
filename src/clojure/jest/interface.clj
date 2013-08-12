36bab57b8c1c3e8104d9b7082c26e6df9c5e91c5
jlicht <jlicht@aerix.nl>
2013-08-12 14:39:13 +0200

    created a simple gui to load levels

--- src/clojure/jest/interface.clj
+++ src/clojure/jest/interface.clj
@@ -1,5 +1,7 @@
 (ns jest.interface
   (:require [seesaw.bind :as b])
+  (:require [jest.level :as level])
+  (:require [me.raynes.fs :as fs])
   (:use seesaw.mig
         seesaw.core))
 
@@ -30,3 +32,41 @@
           watches (for [{:keys [w t tf]} watches]
                     (watcher-label w t tf))]
       (concat fn-buttons watches))))
+
+(defn level-fn [level]
+  (partial level/load-world (fs/absolute-path level)))
+
+(defn levels [dir]
+  (let [level-files (filter #(= (fs/extension %) ".level") (file-seq dir))]
+    (map level-fn level-files)))
+
+(def level-dir (fs/file "levels"))
+(defn level-conf [i f]
+  {:f f :t (str "Level " i)})
+
+(def conf {:fn-s (vec (map-indexed level-conf (levels level-dir)))})
+
+
+;; (def ex-conf
+;;   {:fn-s [{:f pause! :t "Pause"}
+;;            {:f resume! :t "Resume"}
+;;            {:f (level-helper [:tutorial 0]) :t "Level 0"}
+;;            {:f (level-helper [:tutorial 1]) :t "Level 1"}]
+;;    :watches [{:w world-sketch, :t "World Sketch"}
+;;              {:w world-bricklet, :t "World bricklet"}]})
+
+;; (defn demo-conf []
+;;   {:fn-s (vec (concat [{:f pause! :t "Pause"}
+;;                        {:f resume! :t "Resume"}
+;;                        {:f start-spawning :t "Start spawning"}
+;;                        {:f stop-spawning :t "Stop spawning"}]
+;;                       (map-indexed
+;;                        (fn [i f] {:f f :t (str "Level " i)})
+;;                        (:tutorial levels))))
+;;    :watches (vec (concat (if-let [wb @world-bricklet]
+;;                            [{:w (:command-queue wb) :t "Command-queue" :tf count}])))})
+
+;; usage: (temp-gui (create-control-panel (demo-conf)))
+
+
+
