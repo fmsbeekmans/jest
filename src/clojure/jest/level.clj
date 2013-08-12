bbae898507f1a73ff78feefad1a54f6a0ce5cefc
jlicht <jlicht@aerix.nl>
2013-08-12 13:49:25 +0200

    moved level functionality from user to level.clj

--- src/clojure/jest/level.clj
+++ src/clojure/jest/level.clj
@@ -1,8 +1,10 @@
 (ns jest.level
-  (:require [jest.world :as world :refer [reset-world]]
+  (:require [jest.world :as world :refer [all-cells reset-world world-width world-height]]
+            [jest.world.building :refer [spawn? building-type all-spawns all-supplies all-mixers all-depots all-restricteds]]
             [jest.scheduler :refer [scheduler-reset! start! pause!]]
             [jest.movement :refer [start-spawning stop-spawning
                                    set-done-callback! reset-done-callback!]]
+            [jest.world.path :refer [out-paths]]
             [jest.score :refer [reset-score]]
             [clojure.core.incubator :refer [-?>]]
             [jest.visualize.visualize :refer [visible]]))
@@ -38,3 +40,55 @@
 
 (defn reset-level []
   (start-level @current-level))
+
+;; World saving
+(defn world-size-definition []
+  `(initialize-world ~(world-width) ~(world-height)))
+
+(defn path-definition-for [p]
+  `(build-path (cell ~(:coords p)) ~(:direction p) ~(:type p)))
+
+(defn extract-path-definitions []
+  (map path-definition-for (flatten (map out-paths (all-cells)))))
+
+(defn print-path-definitions []
+  (doseq [pd (extract-path-definitions)]
+    (println pd)))
+
+(defn building-for [c]
+  (let [type (building-type c)
+        form
+        `(~(symbol (str "build-" (subs (str  type) 1))) (cell ~(:coord c))
+          ~@(case type
+              :spawn [(:vehicle-type c)]
+              :supply [(:resource-type c)]
+              :mixer []
+              :restricted []
+              :depot [(:resource-type c) (:quotum c)]))]
+    (if (and (spawn? c)
+             (:spawning? c))
+      `(enable-spawner ~form ~(:spawn-offset c) ~(:spawn-rate c))
+      form)))
+
+(defn extract-building-definitions []
+  (map building-for (concat (all-spawns)
+                            (all-supplies)
+                            (all-mixers)
+                            (all-depots)
+                            (all-restricteds))))
+
+(defn print-level []
+  (doseq [e (concat [(world-size-definition)]
+             (extract-building-definitions)
+             (extract-path-definitions))]
+    (println e)))
+
+(defn save-world [file-name]
+  (spit file-name (with-out-str (print-level))))
+
+;; This is probably the most evil thing I have ever done. So happy my name is still not correctly tied to this commit ~J
+(defn load-world [file-name]
+  (load-file file-name))
+
+(defn level-helper [level]
+  (partial load-world (str "levels/" level ".level")))
