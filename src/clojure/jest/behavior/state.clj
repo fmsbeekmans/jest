8bde445862486a8bca5a1f3ee1d45e5c3001cbb3
Matthijs van Otterdijk <matko@pievolution.org>
2013-08-15 13:10:15 +0200

    Docstrings for all vehicle behavior state functions.

--- src/clojure/jest/behavior/state.clj
+++ src/clojure/jest/behavior/state.clj
@@ -6,27 +6,36 @@
             [jest.score :refer [score-vehicle]]
             [jest.behavior.routing :refer [update-preferred-path]]))
 
-(defn vehicle-state-in-cell [v]
+(defn vehicle-state-in-cell
+  "Returns :incoming if the vehicle is in the first half of the cell, :outgoing if it is in the second half."
+  [v]
   (let [halfpoint (+ (:entry-time v)
                      (/ (vehicle->duration v) 2))]
     (if (< @game-time halfpoint)
       :incoming
       :outgoing)))
 
-(defn incoming? [v]
+(defn incoming?
+  "Returns true if the vehicle is in the first half of the cell, false otherwise."
+  [v]
   (= (vehicle-state-in-cell v) :incoming))
 
-(defn outgoing? [v]
+(defn outgoing?
+  "Returns true if the vehicle is in the second half of the cell, false otherwise."
+  [v]
   (= (vehicle-state-in-cell v) :outgoing))
 
-(defn- set-end-state [id state]
+(defn- set-end-state
+  "Changes the state of a vehicle and clears its exits."
+  [id state]
   (dosync
    (vehicle-state-change id state)
    (update-vehicle id vehicle-clear-exit)))
 
 (defn start-despawning
-  "Modifies the state of the vehicle with the given id to :despawning, and
-   schedules removal from the map."
+  "Modifies the state of the vehicle with the given id to :despawning,
+   and clears the exit of this vehicle, ensuring that it will be
+   removed later."
   [id]
   {:pre [(spawn? (vehicle-cell (vehicle id)))]}
   (dosync
@@ -34,18 +43,27 @@
    (set-end-state id :despawning)))
 
 (defn start-exploding
-  "Modifies the state of the vehicle with the given id to :exploding."
+  "Modifies the state of the vehicle with the given id to :exploding,
+  and clears the exit of this vehicle, ensuring that it will be
+  removed later."
   [id]
   (if (spawning? (vehicle id))
     (set-end-state id :spawning-exploding)
     (set-end-state id :exploding)))
 
-(defn- maybe-explode [id]
+(defn- maybe-explode
+  "When an exit direction is missing from this vehicle and the vehicle
+  isn't currently in a spawn point, start exploding."
+  [id]
   (when-not (or (:exit-direction (vehicle id))
                 (spawn? (vehicle-cell (vehicle id))))
     (start-exploding id)))
 
-(defn update-vehicle-exit [id]
+(defn update-vehicle-exit
+  "Sets a vehicle's exit direction to its preferred path. If there's
+  no outgoing path (and the vehicle is not on a spawn point), start
+  exploding."
+  [id]
   (update-vehicle id update-preferred-path)
   (maybe-explode id))
 
