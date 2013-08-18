fb1e9d2f4ec92569d7f4cda12593dbfa6c18c801
Matthijs van Otterdijk <matko@pievolution.org>
2013-08-18 23:06:01 +0200

    Editor docstrings.

--- src/clojure/jest/input/editor.clj
+++ src/clojure/jest/input/editor.clj
@@ -1,4 +1,5 @@
 (ns jest.input.editor
+  "Alternative interaction functions for level editing."
   (:require [jest.input.core :refer [set-input-handler!]]
             [jest.input.interaction :as i]
             [jest.world :refer [cell]]
@@ -7,13 +8,21 @@
                                          depot? build-depot unbuild-depot
                                          mixer? build-mixer unbuild-mixer
                                          building-type build-restricted
-                                         unbuild-restricted]]))
+                                         unbuild-restricted]]
 
-(defn disable-editor []
+            [clojure.core.incubator :refer [defmacro-]]))
+
+(defn disable-editor
+  "Disables the editor - restores normal interaction functions."
+  []
   (i/interaction-setup))
 
-(defmacro editor [name [& args] & body]
-  `(defn ~(symbol (str name "-editor")) [~@args]
+(defmacro- editor
+  "Helper macro for creating the various editor functions."
+  [name [& args] & body]
+  `(defn ~(symbol (str name "-editor"))
+     ~(str "Returns a map of interaction handlers to be used with set-editor to build a " name ".")
+     [~@args]
      (let [tracker# (atom {})]
        {:on-down (fn [id# pos#]
                    (swap! tracker# assoc id# pos#))
@@ -21,15 +30,21 @@
                  (if (= ~'pos (@tracker# id#))
                    ~@body))})))
 
-(defn set-editor [handlers]
+(defn set-editor
+  "Enables an editor. handlers should be the output from one of the editor functions."
+  [handlers]
   (doseq [[type handler] handlers]
     (set-input-handler! type handler)))
 
-(defn unbuild-any-building [c]
+(defn unbuild-any-building
+  "Unbuilds whatever building is on the given cell."
+  [c]
   (if-let [type (building-type c)]
     (@(ns-resolve 'jest.world.building (symbol (str "unbuild-" (subs (str type) 1)))) c)))
 
-(defmacro building-editor [type & args]
+(defmacro- building-editor
+  "Helper macro for creating building editors."
+  [type & args]
   `(editor ~type [~@args]
            (let [c# (cell ~'pos)]
              (if (building-type c#)
@@ -42,5 +57,7 @@
 (building-editor mixer)
 (building-editor restricted)
 
-(defmacro edit [type & args]
+(defmacro edit
+  "Starts the editor for the given edit type. Args are editor type specific."
+  [type & args]
   `(set-editor (~(symbol "jest.input.editor" (str type "-editor")) ~@args)))
